2c75144ff26705badc4c23c80ecad25040d2422d
jabrahamson9 <jcabrahamson9@gmail.com>
2020-12-07 17:45:47 -0700

    getting power value

--- equityAnal.py
+++ equityAnal.py
@@ -7,23 +7,26 @@ import numpy as np
 
 tickerlist = ['LULU', 'TSN']
 
-def RSI(ticker, daysBack, DATA):
+def RSI(ticker, DATA):
     # DATA = pdr.get_data_yahoo(ticker,
     #                         start = datetime.datetime.today() - timedelta(days=daysBack+14),
     #                         end = datetime.datetime.today())
     DATA.index = pd.to_datetime(DATA.index).strftime('%Y-%m-%d')
     avgUp = []
     avgDown = []
-    for i in range(daysBack):
+    for i in range(len(DATA)-14):
         up = []
         down = []
         prevClose = DATA.at[DATA.index[i], 'Close']
+        # print(prevClose)
         base = datetime.datetime.strptime(DATA.index[i], '%Y-%m-%d')
         date_list = [base + timedelta(days=x) for x in range(14)]
         # print(date_list)
+        count = 0
         for index, row in DATA.iterrows():
             index = datetime.datetime.strptime(index, '%Y-%m-%d')
             if index in date_list:
+                count = count + 1
                 if row['Close'] > prevClose:
                     up.append(row['Close']-prevClose)
                 elif row['Close'] < prevClose:
@@ -39,15 +42,15 @@ def RSI(ticker, daysBack, DATA):
             avgDown.append(sum(down)/len(down))
     RSI = []
     for x in range(len(avgUp)):
-        RSvalue = (avgUp[x] / avgDown[x])
+        if avgDown[x] != 0:
+            RSvalue = (avgUp[x] / avgDown[x])
+        else:
+            Svalue = (avgUp[x] / 1)
         RSI.append(100 - (100 / (1+RSvalue)))
     return RSI
 
 
-def StoOsc(ticker, daysBack, DATA):
-    # DATA = pdr.get_data_yahoo(ticker,
-    #                         start = datetime.datetime.today() - timedelta(days=daysBack),
-    #                         end = datetime.datetime.today())
+def StoOsc(ticker, DATA, graph):
     DATA['L14'] = DATA['Low'].rolling(window=14).min()
     #Create the "H14" column in the DataFrame
     DATA['H14'] = DATA['High'].rolling(window=14).max()
@@ -55,16 +58,14 @@ def StoOsc(ticker, daysBack, DATA):
     DATA['%K'] = 100*((DATA['Close'] - DATA['L14']) / (DATA['H14'] - DATA['L14']) )
     #Create the "%D" column in the DataFrame
     DATA['%D'] = DATA['%K'].rolling(window=3).mean()
-    fig, axes = plt.subplots(nrows=2, ncols=1,figsize=(15,7))
-    DATA['Close'].plot(ax=axes[0]); axes[0].set_title('Close')
-    DATA[['%K','%D']].plot(ax=axes[1]); axes[1].set_title('Oscillator')
-    #plt.show()
+    if graph == True:
+        fig, axes = plt.subplots(nrows=2, ncols=1,figsize=(15,7))
+        DATA['Close'].plot(ax=axes[0]); axes[0].set_title('Close')
+        DATA[['%K','%D']].plot(ax=axes[1]); axes[1].set_title('Oscillator')
+        plt.show()
     return list(DATA['%K']), list(DATA['%D'])
 
-def MA(ticker, daysBack, DATA):
-    # DATA = pdr.get_data_yahoo(ticker,
-    #                         start = datetime.datetime.today() - timedelta(days=daysBack),
-    #                         end = datetime.datetime.today())
+def MA(ticker, DATA, graph):
     short_window = 5
     long_window = 30
     # Initialize the `signals` DataFrame with the `signal` column
@@ -79,39 +80,38 @@ def MA(ticker, daysBack, DATA):
     signals['signal'][short_window:] = np.where(signals['short_mavg'][short_window:] > signals['long_mavg'][short_window:], 1.0, 0.0)   
     # Generate trading orders
     signals['positions'] = signals['signal'].diff()
-    # Initialize the plot figure
-    fig = plt.figure(figsize=(6, 4))
-    # Add a subplot and label for y-axis
-    ax1 = fig.add_subplot(111,  ylabel='Price in $')
-    DATA['Close'].plot(ax=ax1, color='r', lw=2.0)
-    signals[['short_mavg', 'long_mavg']].plot(ax=ax1, lw=2.0)
-    # Plot the buy signals
-    ax1.plot(signals.loc[signals.positions == 1.0].index, 
-                signals.short_mavg[signals.positions == 1.0],
-                '^', markersize=10, color='m')
-    # Plot the sell signals
-    ax1.plot(signals.loc[signals.positions == -1.0].index, 
-                signals.short_mavg[signals.positions == -1.0],
-                'v', markersize=10, color='k')
-    #plt.show()
+    if graph == True:
+        # Initialize the plot figure
+        fig = plt.figure(figsize=(6, 4))
+        # Add a subplot and label for y-axis
+        ax1 = fig.add_subplot(111,  ylabel='Price in $')
+        DATA['Close'].plot(ax=ax1, color='r', lw=2.0)
+        signals[['short_mavg', 'long_mavg']].plot(ax=ax1, lw=2.0)
+        # Plot the buy signals
+        ax1.plot(signals.loc[signals.positions == 1.0].index, 
+                    signals.short_mavg[signals.positions == 1.0],
+                    '^', markersize=10, color='m')
+        # Plot the sell signals
+        ax1.plot(signals.loc[signals.positions == -1.0].index, 
+                    signals.short_mavg[signals.positions == -1.0],
+                    'v', markersize=10, color='k')
+        plt.show()
     return list(signals['positions']), list(signals['short_mavg']), list(signals['long_mavg'])
 
-def Boll(ticker, daysBack, DATA):
-    # DATA = pdr.get_data_yahoo(ticker,
-    #                         start = datetime.datetime.today() - timedelta(days=daysBack),
-    #                         end = datetime.datetime.today())
+def Boll(ticker, DATA, graph):
     fig = plt.figure(figsize=(6, 4))
     ax1 = fig.add_subplot(111,  ylabel='Price in $')
     signals_two = pd.DataFrame(index=DATA.index)
     signals_two['upper_BB'] = DATA['Close'].rolling(window=10, min_periods=1, center=False).mean() + 2 * DATA['Close'].rolling(window=10, min_periods=1, center=False).std()
     signals_two['lower_BB'] = DATA['Close'].rolling(window=10, min_periods=1, center=False).mean() - 2 * DATA['Close'].rolling(window=10, min_periods=1, center=False).std()
-    ax = signals_two[['upper_BB', 'lower_BB']].plot(ax=ax1, lw=1, alpha=0.4, color="black")
-    ax.fill_between(DATA.index, signals_two['upper_BB'], signals_two['lower_BB'], color='#ADCCFF', alpha='0.4')
-    DATA['Close'].plot(ax=ax1, color='r', lw=2.0)
-    plt.show()
+    if graph == True:
+        ax = signals_two[['upper_BB', 'lower_BB']].plot(ax=ax1, lw=1, alpha=0.4, color="black")
+        ax.fill_between(DATA.index, signals_two['upper_BB'], signals_two['lower_BB'], color='#ADCCFF', alpha='0.4')
+        DATA['Close'].plot(ax=ax1, color='r', lw=2.0)
+        plt.show()
     return list(signals_two['upper_BB']), list(signals_two['lower_BB']), list(DATA['Close'])
 
-def stockVal(ticker, daysBack):
+def stockVal(ticker, daysBack, graph):
     DATA = pdr.get_data_yahoo(ticker,
                             start = datetime.datetime.today() - timedelta(days=daysBack),
                             end = datetime.datetime.today())   
@@ -124,31 +124,62 @@ def stockVal(ticker, daysBack):
     MA_Val = 0
     RSI_Val = 0
 
-    RSI = RSI(tickerlist, daysBack, DATA_RSI)
-    K,D = StoOsc(tickerlist, daysBack, DATA)
-    sig, shortMA, longMA = MA(tickerlist, daysBack, DATA)
-    up, low, close = Boll(tickerlist, daysBack, DATA)
+    RSI_arr = RSI(tickerlist, DATA_RSI)
+    K,D = StoOsc(tickerlist, DATA, graph)
+    sig, shortMA, longMA = MA(tickerlist, DATA, graph)
+    up, low, close = Boll(tickerlist, DATA, graph)
 
     prev_short = shortMA[0]
     prev_long = longMA[0]
     prev_K = K[0]
     prev_D = D[0]
-    for i in range(close):
-        if close[i] > up[i]:
-            Boll_Val = -1
-        elif close[i] < low[i]:
-            Boll_Val = 1
 
+    if close[-1] > up[-1]:
+        Boll_Val = -1
+    elif close[-1] < low[-1]:
+        Boll_Val = 1
+
+    if RSI_arr[-1] >= 70:
+        RSI_Val = -1 + ((100 - RSI_arr[-1]) * 0.01)
+    elif RSI_arr[-1] <= 20:
+        RSI_Val = 1 - (20 - (RSI_arr[-1]) * 0.01)
+
+    MA_idx = 0
+    STO_idx = 0
+    for i in range(len(close)):
         if shortMA[i] > longMA[i] and prev_short < prev_long:
             MA_Val = 1
+            MA_idx = i
+
         elif shortMA[i] < longMA[i] and prev_short > prev_long:
             MA_Val = -1
+            MA_idx = i
         prev_short = shortMA[i]
         prev_long = longMA[i]
 
         if K[i] < D[i] and prev_K > prev_D and K[i] >= 80:
             Sto_Val = -1
+            STO_idx = i
         elif K[i] > D[i] and prev_K < prev_D and K[i] <= 20:
             Sto_Val = 1
+            STO_idx = i
+        prev_K = K[i]
+        prev_D = D[i]
 
-stockVal('LULU', 365)
+    if MA_Val == 1:
+        MA_Val = MA_Val - ((len(close)-MA_idx) * 0.01)
+    elif MA_Val == -1:
+        MA_Val = MA_Val + ((len(close)-MA_idx) * 0.01)
+
+    if Sto_Val == 1:
+        Sto_Val = Sto_Val - ((len(close)-STO_idx) * 0.01)
+    elif Sto_Val == -1:
+        Sto_Val = Sto_Val + ((len(close)-STO_idx) * 0.01)
+    
+    print(Boll_Val, Sto_Val, MA_Val, RSI_Val)
+    
+    power = Boll_Val + Sto_Val + MA_Val + RSI_Val
+    return power
+
+power = stockVal('MSFT', 365, True)
+print(power)
