3f709f2bce179f960bef3ffc733fc2d1a53c615f
Kevin Ruby <kevinruby@Kevins-MacBook-Pro.local>
2020-12-09 14:13:32 -0700

    pushing

--- equityAnal2.py
+++ equityAnal2.py
@@ -4,7 +4,15 @@ from datetime import timedelta
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from operator import itemgetter 
+from operator import itemgetter
+import tkinter as tk
+
+import glob
+import os
+import re
+from reportlab.lib import utils
+from reportlab.pdfgen import canvas
+from unite_multiple_pictures_into_pdf import sorted_nicely, unite_pictures_into_pdf
 
 def RSI(ticker, DATA):
     DATA.index = pd.to_datetime(DATA.index).strftime('%Y-%m-%d')
@@ -48,6 +56,7 @@ def StoOsc(ticker, DATA, graph):
         fig, axes = plt.subplots(nrows=2, ncols=1,figsize=(15,7))
         DATA['Close'].plot(ax=axes[0]); axes[0].set_title('Close')
         DATA[['%K','%D']].plot(ax=axes[1]); axes[1].set_title('Oscillator')
+        plt.title(ticker + " Stochastic Oscillator - 1 Year ")
         plt.show()
         filename = str(ticker) + "_STO.png"
         fig.savefig(r"Results/%s" %(filename))
@@ -83,6 +92,7 @@ def MA(ticker, DATA, graph):
         ax1.plot(signals.loc[signals.positions == -1.0].index, 
                  signals.short_mavg[signals.positions == -1.0],
                  'v', markersize=10, color='k')
+        plt.title(ticker + " LMA vs SMA - 1 Year ")
         plt.show()
         filename = str(ticker) + "_MA.png"
         fig.savefig(r"Results/%s" %(filename))
@@ -98,6 +108,8 @@ def Boll(ticker, DATA, graph):
         ax = signals_two[['upper_BB', 'lower_BB']].plot(ax=ax1, lw=1, alpha=0.4, color="black")
         ax.fill_between(DATA.index, signals_two['upper_BB'], signals_two['lower_BB'], color='#ADCCFF', alpha='0.4')
         DATA['Close'].plot(ax=ax1, color='r', lw=2.0)
+        plt.title(ticker + " Bollinger Band - 1 Year ")
+
         plt.show()
         filename = str(ticker) + "_BOLL.png"
         fig.savefig(r"Results/%s" %(filename))
@@ -185,7 +197,7 @@ tickerlist = ['FATE', 'OPTT', 'VXRT', 'IGMS']
 
 file1 = open('recommendations.txt', "r")
 
-  
+
 
 def results(ticker):
     DATA = pdr.get_data_yahoo(ticker,
@@ -194,24 +206,134 @@ def results(ticker):
     fig = plt.figure(figsize=(6, 4))
     ax1 = fig.add_subplot(111,  ylabel='Price in $')
     DATA['Close'].plot(ax=ax1, color='r', lw=2.0)
-    if ticker == 'OPTT':
-        plt.axvline(datetime.datetime(2019, 11, 15))
-    else:
-        plt.axvline(datetime.datetime(2019, 11, 9))
+    plt.axvline(datetime.datetime(2019, 11, 9))
     plt.title(ticker + " Performance Analysis")
     plt.show()
     filename = str(ticker) + "_ANALYSIS.png"
     fig.savefig(filename)
 
+def create_report(user_email):
+    
+    subject = "An email with attachment from Python"
+    body = "This is an email with attachment sent from Python"
+    sender_email = "stockanalysis553@gmail.com"
+    receiver_email = user_email
+    password = "stocksarecool9853!"
+     # Create a multipart message and set headers
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
+    # message["Bcc"] = receiver_email  # Recommended for mass emails
+    
+    # Add body to email
+    message.attach(MIMEText(body, "plain"))
+    
+    filename = "FinalAnalysis.pdf"  # In same directory as script
+    
+    # Open PDF file in binary mode
+    filepath = os.getcwd() + '/Results/' + filename
+    with open(filepath, "rb") as attachment:
+        # Add file as application/octet-stream
+        # Email client can usually download this automatically as attachment
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+    
+    # Encode file in ASCII characters to send by email    
+    encoders.encode_base64(part)
+    
+    # Add header as key/value pair to attachment part
+    part.add_header("Content-Disposition",f"attachment; filename= {filename}",)
+    
+     # Add attachment to message and convert message to string
+    message.attach(part)
+    text = message.as_string()
+    
+     # Log in to server using secure context and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login("stockanalysis553", password)
+        server.sendmail(sender_email, receiver_email, text)
+ 
+def callPDFSaver():
+    outputPdfName = "FinalAnalysis"
+    pathToSavePdfTo = os.getcwd() + '//' + "Results"
+    pathToPictures = os.getcwd() + '//' + "Results"
+    splitType = "picture"
+    numberOfEntitiesInOnePdf = 20
+    listWithImagesExtensions = ["png"]
+    picturesAreInRootFolder = True
+    nameOfPart = "ALL"
+    unite_pictures_into_pdf(outputPdfName, pathToSavePdfTo, pathToPictures, splitType, numberOfEntitiesInOnePdf, listWithImagesExtensions, picturesAreInRootFolder, nameOfPart)
 
-def main(tickerlist):
+def cleanFolder():
+    pathToFolder = os.getcwd() + '//' + "Results"
+    files = glob.glob(pathToFolder)
+    for f in files:
+        os.remove(f)
+
+def mainUI():
+    def extractTickers(t):
+        print('extracting')
+        return t.split(',')
+
+    def runProgram():
+        print("Email: %s\nStock List: %s" % (e1.get(), e2.get()))
+        email = e1.get()
+        tickers = extractTickers(e2.get())
+        #cleanFolder()
+
+        for ticker in tickers:
+            power = stockVal(ticker, 365, True)
+            print(ticker, ": ", power)
+        #make long PDF
+        callPDFSaver()
+        # #get recommendations
+        # vals = {}
+        # for x in file1:
+        #     # x = file1.readline()
+        #     x = x[2:-3]
+        #     try:
+        #         power = stockVal(x, 365, False)
+        #         vals[x] = power
+        #     except:
+        #         pass
+        # res = dict(sorted(vals.items(), key = itemgetter(1), reverse = True)[:10]) 
+        # print(res) 
+
+        #send email 
+        create_report(email)
 
 
+    
+    master = tk.Tk()
+    tk.Label(master, 
+            text="Email:").grid(row=0)
+    tk.Label(master, 
+            text="List of Tickers: Follow the example.").grid(row=1)
+
+    e1 = tk.Entry(master)
+    v = tk.StringVar(master, value='AAPL,AMZN,TSLA,GE')
+    e2 = tk.Entry(master, textvariable = v)
+
+    e1.grid(row=0, column=1)
+    e2.grid(row=1, column=1)
+
+
+    tk.Button(master, 
+            text='Analyze!', command=runProgram).grid(row=3, column=1, sticky=tk.W, pady=4)
+
+    tk.mainloop()
+
+def main():
+
+
+    mainUI()
     # for ticker in tickerlist:
     #     power = stockVal(ticker, 365, True)
     #     print(ticker, ": ", power)
-
-
+    #make long PDF
+    #get recommendations
     # vals = {}
     # for x in file1:
     #     # x = file1.readline()
@@ -223,13 +345,16 @@ def main(tickerlist):
     #         pass
     # res = dict(sorted(vals.items(), key = itemgetter(1), reverse = True)[:10]) 
     # print(res) 
+    #send email
 
-    for ticker in tickerlist:
-        results(ticker)
+ 
+
+    # for ticker in tickerlist:
+    #     results(ticker)
 
 
 if __name__ == "__main__":
-    main(tickerlist)
+    main()
 
 
 
